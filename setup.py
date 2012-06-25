a447a43bb5ca86cce07e18a3d8b6ec8648d6503b
Charlie Denton <charlie@meshy.co.uk>
2012-06-25 14:15:42 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-file-storage',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/meshy/django-file-storage',
     license='LGPL',
     author='Charlie Denton',
